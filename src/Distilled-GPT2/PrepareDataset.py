ea7be5ffb96db3f30b900fd5a72875e85d842b8c
jkzcodes <jessiekurtz123@gmail.com>
2024-11-29 14:01:33 -0500

    nltk

--- src/Distilled-GPT2/PrepareDataset.py
+++ src/Distilled-GPT2/PrepareDataset.py
@@ -5,6 +5,7 @@ import sys
 from datasets import Dataset  # type: ignore
 from sklearn.metrics import accuracy_score  # type: ignore
 import string
+import nltk.corpus  # type: ignore
 from nltk.corpus import stopwords  # type: ignore
 from nltk.stem import WordNetLemmatizer  # type: ignore
 
