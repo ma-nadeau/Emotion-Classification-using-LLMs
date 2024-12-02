3d6e3a2e4b57eb864f07018d99b45f90538e4857
Marc-Antoine Nadeau <nadeau.marc-antoine@hotmail.com>
2024-12-01 21:00:47 -0500

    fixed

--- Output/launch.sh
+++ Output/launch.sh
@@ -18,6 +18,6 @@ module load miniconda/miniconda-fall2024
 
 # wait
 
-# python ../../Emotion-Classification-using-LLMs/src/Distilled-GPT2/PrepareDataset.py
-python ../../Emotion-Classification-using-LLMs/src/BERT/PrepareDataset.py 
-python ../../Emotion-Classification-using-LLMs/src/GPT2/PrepareDataset.py
+python ../../Emotion-Classification-using-LLMs/src/Distilled-GPT2/PrepareDataset.py
+# python ../../Emotion-Classification-using-LLMs/src/BERT/PrepareDataset.py 
+# python ../../Emotion-Classification-using-LLMs/src/GPT2/PrepareDataset.py
